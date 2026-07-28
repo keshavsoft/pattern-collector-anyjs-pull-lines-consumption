20ac0cb429d8d2caddc55119cb37ab0364f4b421
keshavsoft <nknnkumar@yahoo.com>
2026-07-28 21:36:53 +0530

    3 in type

---
+++ test/v3/test.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from "url";
+
+import defaultFunc from '../../index.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const appJsPath = path.join(__dirname, "routes.js");
+
+const fileContent = fs.readFileSync(appJsPath, 'utf8');
+
+import extractRegex from './extractRegex.js';
+
+const k1 = defaultFunc({
+    fileContent,
+    parseRegex: extractRegex?.fromRoutesJs.consumptionRegex.parseRegex,
+    searchRegex: extractRegex?.fromRoutesJs.consumptionRegex.searchRegex,
+    fileType: "routesJs",
+    showLog: {
+        keysOnly: false,
+        withValues: false
+    },
+    showLogStep1: {
+        keysOnly: false,
+        withValues: false
+    }
+});
+
+console.log("ssssssssss : ", k1);
+
